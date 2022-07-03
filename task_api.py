9e1b15f13a86a30bae42587c07f60d4ef52d37fe
ronwagnerjudo <ron.wagner.judo@gmail.com>
2022-07-03 17:16:51 +0300

    adding logs

--- task_api.py
+++ task_api.py
@@ -53,19 +53,25 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-  
+        logging.info("Searching for jwt")
         if request.cookies.get("jwt"):
+            logging.info("JWT found")
             token = request.cookies.get("jwt")
 
         if not token:
+            logging.info("No Token")
             return jsonify({'message' : 'Token is missing!'}), 401
 
         try: 
             response = requests.get("http://127.0.0.1:5000/get-user-details", cookies=request.cookies)
-            data = response.json()
-            current_user = data['user_details']['sub']
+            logging.info("Sent get request to user api")
         except:
+            logging.info("Problem with the get-user-details response")
             return jsonify({"message' : 'couldn't get a response"}), 404
+
+        data = response.json()
+        current_user = data['user_details']['sub']
+
 
         return f(current_user, *args, **kwargs)
 
@@ -74,7 +80,7 @@ def token_required(f):
 @app.route("/all")
 @token_required
 def all_tasks(current_user):
-    logging.info("querying all task in db")
+    logging.info("Querying all task in db")
     all_tasks = TasksSprintManager.query.filter_by(sub=current_user).all()
     return jsonify(tasks=[task.to_dict() for task in all_tasks])
 
@@ -86,7 +92,7 @@ def add_tasks(current_user):
     data = json.loads(request.data)
     task_name = data["task_name"]
     task_time = data["task_time"]
-    logging.info("getting data from the front-end")
+    logging.info("Getting data from the front-end")
 
     if task_name != "" or task_time != "":
         task_params = {
@@ -94,12 +100,16 @@ def add_tasks(current_user):
             "task_time": task_time
         }
 
-        logging.info("init post request")
-        response = requests.post(f"{CALENDER_API_URL}/new_task", json=task_params, cookies=request.cookies)
-        logging.info("sent post request to calendar api")
+        logging.info("Init post request")
+        try:
+            response = requests.post(f"{CALENDER_API_URL}/new_task", json=task_params, cookies=request.cookies)
+            logging.info("Sent post request to calendar api")
+        except:
+            logging.info("Problem with the calendar_api/new_task response")
+            return jsonify({"message": "Problem with response."})
 
         if response.status_code == 200:
-            logging.info("getting the id of the event from calendar api")
+            logging.info("Getting the id of the event from calendar api")
             response = response.json()["event"]
             google_event_id = response["googleEventId"]
             task_start_datetime_string = response["start"]["dateTime"]
@@ -114,9 +124,10 @@ def add_tasks(current_user):
             )
             db.session.add(add_new_task)
             db.session.commit()
-            logging.info("added new task to the db")
+            logging.info("Added new task to the db")
             return jsonify({"success": "Successfully added new task."}), 200
         else:
+            logging.info("The response to calendar_api/new_task was not 200.")
             return jsonify({"error": "Couldn't add new task to google calendar"}), response.status_code
     else:
         return jsonify({"Not valid": "Sorry, you can't leave fields empty."}), 404
@@ -128,27 +139,33 @@ def delete_task(current_user):
     data = json.loads(request.data)
     task_id = data["id"]
     task_to_delete = TasksSprintManager.query.filter_by(id=task_id, sub=current_user).first()
-    logging.info("finding the task by id and user sub")
-    if task_to_delete:         
+    logging.info("Finding the task by id and user sub")
+    if task_to_delete:
+        logging.info("Found the task by id and user sub")       
 
         task_params = {
         "googleEventId": task_to_delete.google_event_id
         }
 
-        logging.info("init delete request")
-        response = requests.delete(f"{CALENDER_API_URL}/delete", json=task_params, cookies=request.cookies)
-        
-        
+        logging.info("Init delete request")
+        try:
+            response = requests.delete(f"{CALENDER_API_URL}/delete", json=task_params, cookies=request.cookies)
+            logging.info("Sent delete request to calendar api")
+        except:
+            logging.info("Problem with the calendar_api/delete response")
+            return jsonify({"message": "Problem with response."})
+
         if response.status_code == 200:
-            logging.info("sent delete request to calendar api")
+            logging.info("Sent delete request to calendar api")
             db.session.delete(task_to_delete)
             db.session.commit()
-            logging.info("task deleted in db")
+            logging.info("Task deleted in db")
             return jsonify({"success": "Successfully deleted task."}), 200
         else:
+            logging.info("The response to calendar_api/delete was not 200.")
             return jsonify({"error": "Event did not deleted in the google calendar."}), response.status_code
     else:
-        logging.info("couldn't find the task")
+        logging.info("Couldn't find the task")
         return jsonify({"Not Found": "Sorry a task with that id or sub was not found in the database."}), 404
 
 
@@ -159,16 +176,14 @@ def update_task(current_user):
 
     task_name = data["task_name"]
     task_time = data["task_time"]
-    logging.info("getting data from the front-end")
+    task_id = data["id"]
+    logging.info("Getting data from the front-end")
 
-    task_id = request.form.get("id")
-    logging.info("querying db to find the task by id")
+    logging.info("Querying db to find the task by id and user sub")
     task_to_update = TasksSprintManager.query.filter_by(id=task_id, user_id=current_user).first()
+    logging.info("Finding the task by id and user sub")
     if task_to_update:
-        task_to_update.task_name = task_name
-        task_to_update.task_time = task_time
-        db.session.commit()
-        logging.info("updated task in db")
+        logging.info("Found the task by id and user sub")
 
         task_params = {
         "task_name": task_name,
@@ -176,12 +191,26 @@ def update_task(current_user):
         "googleEventId": task_to_update['google_event_id']
         }
 
-        logging.info("init put request")
-        requests.put(f"{CALENDER_API_URL}/new_task", json=task_params)
-        logging.info("sent put request to calendar api")
-        return jsonify({"success": "Successfully updated the new task."}), 200
+        logging.info("Init put request")
+        try:
+            response = requests.put(f"{CALENDER_API_URL}/update", json=task_params)
+            logging.info("Sent put request to calendar api")
+        except:
+            logging.info("Problem with the calendar_api/update response")
+            return jsonify({"message": "Problem with response."})
+
+        if response.status_code == 200:
+            logging.info("Sent put request to calendar api")
+            task_to_update.task_name = task_name
+            task_to_update.task_time = task_time
+            db.session.commit()
+            logging.info("Updated task in db")
+            return jsonify({"success": "Successfully updated the new task."}), 200
+        else:
+            logging.info("The response to calendar_api/update was not 200.")
+            return jsonify({"error": "Event did not updated in the google calendar."}), response.status_code
     else:
-        logging.info("task did not found in db")
+        logging.info("Task did not found in db")
         return jsonify(error={"Not Found": "Sorry a task with that id was not found in the database."}), 404
 
 
